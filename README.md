5c524904d2106d5b0dca0e49fabf3bc1181d9e21
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2023-03-15 11:01:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Playwright 2FA Authentication with AAD or MSA
+
+This sample demonstrates how to use the Playwright C# SDK to automatically authenticate against a web application which is protected by either Azure Active Directory or a Microsoft Account which has two-factor authentication.
+
+## Configuring your 2FA
+
+### AAD
+
+TBD
+
+### MSA
+
+TBD
+
+## Licenses
+
+[![GitHub license](https://img.shields.io/badge/License-Apache%202-blue.svg)](https://raw.githubusercontent.com/endjin/playwright-two-factor-auth-csharp/main/LICENSE)
+
+This sample is available under the Apache 2.0 open source license.
+
+For any licensing questions, please email [&#108;&#105;&#99;&#101;&#110;&#115;&#105;&#110;&#103;&#64;&#101;&#110;&#100;&#106;&#105;&#110;&#46;&#99;&#111;&#109;](&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#108;&#105;&#99;&#101;&#110;&#115;&#105;&#110;&#103;&#64;&#101;&#110;&#100;&#106;&#105;&#110;&#46;&#99;&#111;&#109;)
+
+## Project Sponsor
+
+This project is sponsored by [endjin](https://endjin.com), a UK-based technology consultancy who are corporate sponsors of the .NET Foundation.
+
+For more information about our products and services, or for commercial support of this project, please [contact us](https://endjin.com/contact-us). 
+
+We produce two free weekly newsletters; [Azure Weekly](https://azureweekly.info) for all things about the Microsoft Azure Platform, and [Power BI Weekly](https://powerbiweekly.info).
+
+Keep up with everything that's going on at endjin via our [blog](https://blogs.endjin.com/), follow us on [Twitter](https://twitter.com/endjin), or [LinkedIn](https://www.linkedin.com/company/1671851/).
+
+Our other Open Source projects can be found on [GitHub](https://github.com/endjin)
+
+## Code of conduct
+
+This project has adopted a code of conduct adapted from the [Contributor Covenant](http://contributor-covenant.org/) to clarify expected behavior in our community. This code of conduct has been [adopted by many other projects](http://contributor-covenant.org/adopters/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [&#104;&#101;&#108;&#108;&#111;&#064;&#101;&#110;&#100;&#106;&#105;&#110;&#046;&#099;&#111;&#109;](&#109;&#097;&#105;&#108;&#116;&#111;:&#104;&#101;&#108;&#108;&#111;&#064;&#101;&#110;&#100;&#106;&#105;&#110;&#046;&#099;&#111;&#109;) with any additional questions or comments.
---
+++ Solutions/Playwright2FA/Program.cs
@@ -0,0 +1,78 @@
+﻿// <copyright file="Program.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+using Microsoft.Playwright;
+using OtpNet;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        /*
+        NOTE: In production scenarios you should keep the credentials & secrets in Key Vault NOT Environment Variables.
+        */
+
+        using IPlaywright playwright = await Playwright.CreateAsync();
+        await using IBrowser browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+            SlowMo = 2000 // Add pause to each step so that you can see automation in action!
+        });
+
+        // Configure AAD 2FA: https://mysignins.microsoft.com/security-info
+        await AadSignIn(browser);
+
+        // Configure MSA 2FA: https://account.microsoft.com/security
+        await MsaSignIn(browser);
+    }
+
+    private static async Task AadSignIn(IBrowser browser)
+    {
+        IBrowserContext context = await browser.NewContextAsync();
+        IPage page = await context.NewPageAsync();
+
+        await page.GotoAsync("https://mysignins.microsoft.com/security-info");
+
+        await page.GetByPlaceholder("Email address or phone number").FillAsync(Environment.GetEnvironmentVariable("PLAYWRIGHT_AAD_ACCOUNT_USERNAME")!);
+        await page.GetByRole(AriaRole.Button, new() { Name = "Next" }).ClickAsync();
+
+        // There seem to be different sign-in UIs, you may need this step, depending on the site you're signing into
+        // await page.GetByRole(AriaRole.Button, new() { NameRegex = new Regex("Work or school account Created by your IT department", RegexOptions.IgnoreCase) }).ClickAsync();
+        await page.Locator("#i0118").FillAsync(Environment.GetEnvironmentVariable("PLAYWRIGHT_AAD_ACCOUNT_PASSWORD")!);
+        await page.Locator("#i0118").PressAsync("Enter");
+
+        await page.GetByRole(AriaRole.Textbox, new() { Name = "Enter code" }).FillAsync(GenerateTwoFactorAuthCode(Environment.GetEnvironmentVariable("PLAYWRIGHT_AAD_ACCOUNT_TOTP")!));
+        await page.GetByLabel("Don't ask").CheckAsync();
+        await page.GetByRole(AriaRole.Button, new() { Name = "Verify" }).ClickAsync();
+        await page.GetByText("Don't show this again").ClickAsync();
+        await page.GetByRole(AriaRole.Button, new() { Name = "Yes" }).ClickAsync();
+    }
+
+    private static async Task MsaSignIn(IBrowser browser)
+    {
+        IBrowserContext context = await browser.NewContextAsync();
+        IPage page = await context.NewPageAsync();
+
+        await page.GotoAsync("https://login.live.com/");
+        await page.GetByPlaceholder("Email, phone or Skype").FillAsync(Environment.GetEnvironmentVariable("PLAYWRIGHT_MSA_ACCOUNT_USERNAME")!);
+        await page.GetByRole(AriaRole.Button, new() { Name = "Next" }).ClickAsync();
+
+        // There seem to be different sign-in UIs, you may need this step, depending on the site you're signing into
+        // await page.GetByRole(AriaRole.Button, new() { NameRegex = new Regex("Personal account Created by you", RegexOptions.IgnoreCase) }).ClickAsync();
+        await page.GetByPlaceholder("Password").FillAsync(Environment.GetEnvironmentVariable("PLAYWRIGHT_MSA_ACCOUNT_PASSWORD")!);
+        await page.GetByRole(AriaRole.Button, new() { Name = "Sign in" }).ClickAsync();
+        await page.GetByPlaceholder("Code").FillAsync(GenerateTwoFactorAuthCode(Environment.GetEnvironmentVariable("PLAYWRIGHT_MSA_ACCOUNT_TOTP")!));
+        await page.GetByLabel("Don't ask me again on this device").CheckAsync();
+        await page.GetByRole(AriaRole.Button, new() { Name = "Verify" }).ClickAsync();
+        await page.GetByText("Don't show this again").ClickAsync();
+        await page.GetByRole(AriaRole.Button, new() { Name = "Yes" }).ClickAsync();
+    }
+
+    private static string GenerateTwoFactorAuthCode(string code)
+    {
+        Totp totp = new(secretKey: Base32Encoding.ToBytes(code));
+        
+        return totp.ComputeTotp();
+    }
+}
